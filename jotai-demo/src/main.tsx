62f1da6563bba040f82b72a674145a1dad4e81cf
w3liangxn <gfr6248@dingtalk.com>
2024-07-16 16:55:12 +0800

    feat(jotai-devtools-demo): devtools 示例

--- jotai-demo/src/main.tsx
+++ jotai-demo/src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { DevTools } from 'jotai-devtools'
+import 'jotai-devtools/styles.css'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <DevTools />
     <App />
   </React.StrictMode>,
 )
--- jotai-demo/vite.config.ts
+++ jotai-demo/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      presets: ['jotai/babel/preset'],
+    },
+  })],
 })
