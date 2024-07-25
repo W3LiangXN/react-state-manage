0be9b3c880c71b7f885d0d265ab27c21461253b1
w3liangxn <gfr6248@dingtalk.com>
2024-07-25 14:04:51 +0800

    feat: 修复 mobx-react

--- mobx-react/vite.config.ts
+++ mobx-react/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: { esbuildOptions: { loader: { ".jsx": "tsx" } } },
+  plugins: [react({ babel: { plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]] } })],
 })
